1d0e571cc32687a980f9e1dad8917229a344ea37
LeslySturgill <alonzol.25@outlook.com>
2026-09-08 14:21:15 -0700

    Initial push
    Pushing th “Basic JavaScript Projects” folder containing your “Project1_expressions_alert” folder to your “JAVASCRIPT-PROJECTS” GitHub repository.

---
+++ Basic JavaScript Projects/Basic_Javascript_Projects_Submission_Assigment/JS/main.js
@@ -0,0 +1,13 @@
+var Sent1 = "This is the beginning of the string"; //Defining the first variable and giving it a string value
+window.alert(Sent1) //Using the method to display a pop-up dialog box with a specified message and an OK button
+
+
+var Sent2 = " and this is the end of the string"; //Defining a second variable and giving it a string value
+
+
+document.write(Sent1 //Concatenating the Sent1 and Sent2 varibles together and using method to display in the screen
+    + Sent2
+);
+
+Name = "What a person types in the name box" //Writing an expression
+document.write(Name); //Using method to display it ib the screen
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+document.write('Hello, world!');
+
+var A = "This is a string";
+document.write(A);
+
+var B = "Good morning"
+window.alert(B);
+
+document.write("\"Good afternoon\", He said");
+
+document.write("\"Be who you are and say what you feel,"
+    + "because those who mind don\'t matter and those who matter don\'t mind.\""
+    + "-Dr. Seuss") ;
+
+var B = ("\"Hello\""
+    + "It\'s me");
+document.write(B)
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+            Daughter = "Penny", Son = "Zorro";
+        document.write (Dad);
+
+var Family = "Menu", Breakfast = "Eggs", Lunch = "Salad", Dinner = "Steak",
+            Dessert = "Cheesecake";
+        document.write (Lunch);
+
+document.write(3 + 3); 
